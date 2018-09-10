dd80f54e38691e99cf323136394767696923fa77
Elad Karako <eladkarako@users.noreply.github.com>
2018-09-10 21:55:08 +0300

    (echo on)

--- 2__list_formats.cmd
+++ 2__list_formats.cmd
@@ -1,4 +1,3 @@
-@echo off
 ::ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="Integrated Camera"
 ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="USB Camera"
 pause
