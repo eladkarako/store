462c043cd807cc88aa1823c79b46b337ca62c295
Elad Karako <eladkarako@users.noreply.github.com>
2019-02-02 00:05:19 +0200

    adding short description, changing to PC-WebCam.

--- 2__list_formats.cmd
+++ 2__list_formats.cmd
@@ -1,3 +1,11 @@
+::-----------this is IBM/Lenovo own (on-screen) integrated camera
 ::ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="Integrated Camera"
-ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="USB Camera"
+
+
+::-----------this is a generic "long-cable" 0.3M-Pixel USB-Camera.
+::ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="USB Camera"
+
+
+::-----------this is a PC-WebCam (USB).
+ffmpeg -y -hide_banner -loglevel "info"   -f dshow -list_options true -i video="USB Video Device"
 pause
