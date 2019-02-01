d4b3528513b16b4174db9e548992896a8d842546
Elad Karako <eladkarako@users.noreply.github.com>
2019-02-02 00:34:07 +0200

    using minimal values for framerate (maximum for resolution) for quicker batch-file operation.

--- ffmpeg_webcam_image_save_to_PNG_using_the_ISO_IMG_filename_format___faster___with_timestamp_overlay___USB_Video_Device.cmd
+++ ffmpeg_webcam_image_save_to_PNG_using_the_ISO_IMG_filename_format___faster___with_timestamp_overlay___USB_Video_Device.cmd
@@ -27,7 +27,7 @@ set "FONT=%FONT:\=/%"
 set "ARGS="
 set  ARGS=%ARGS% -y -hide_banner -loglevel "info" -strict "experimental"
 ::video source arguments
-set  ARGS=%ARGS% -f dshow -video_size "640x480" -r "30" -framerate "30.0" -pixel_format "yuyv422" 
+set  ARGS=%ARGS% -f dshow -video_size "640x480" -r "5" -framerate "5.0" -pixel_format "yuyv422" 
 ::limit length
 set  ARGS=%ARGS% -to "00:00:01.000" 
 ::video source
