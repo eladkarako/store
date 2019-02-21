63fca1b4c4302ef28f9276a09ef5103bf3d64b91
Elad Karako <eladkarako@users.noreply.github.com>
2019-02-21 22:22:07 +0200

    adding a preview that is 180deg rotated, adding video capture (and another for capturing with 180deg rotated).

---
+++ 3__preview_video_180rotated.cmd
@@ -0,0 +1,23 @@
+@echo off
+::for when your camera is upside-down.
+set "ARGS="
+set  ARGS=%ARGS% -hide_banner -loglevel "info" -strict "experimental"
+set  ARGS=%ARGS% -rtbufsize 200M
+set  ARGS=%ARGS% -f dshow 
+::------------------------------------------------------------------------------------------video source arguments - need to use a format that is supported (query all devices and then your selected-device's formats).  -- when switching, uncomment the two-lines.
+::set  ARGS=%ARGS% -video_size "1280x720" -framerate "10.0" -pixel_format "yuyv422"  -i video="Integrated Camera"
+::set  ARGS=%ARGS% -video_size "640x480"  -framerate "15.0" -pixel_format "yuyv422"  -i video="USB Camera"
+set  ARGS=%ARGS% -video_size "640x480"  -framerate "30.0" -pixel_format "yuyv422"  -i video="USB Video Device"
+
+::------------------------------------------------------------------------------------------explicitly avoiding other streams but video.
+set  ARGS=%ARGS% -an -sn
+
+
+set  ARGS=%ARGS% -vf "transpose=cclock,transpose=cclock"
+echo.
+echo ffplay %ARGS%    1>&2
+echo.
+call ffplay %ARGS%
+echo.
+
+pause
---
+++ 4__capture_video.cmd
@@ -0,0 +1,37 @@
+@echo off
+::for when your camera is upside-down.
+set "ARGS="
+set  ARGS=%ARGS% -y -hide_banner -loglevel "info" -strict "experimental"
+set  ARGS=%ARGS% -rtbufsize 200M
+set  ARGS=%ARGS% -f dshow 
+::------------------------------------------------------------------------------------------video source arguments - need to use a format that is supported (query all devices and then your selected-device's formats).  -- when switching, uncomment the two-lines.
+::set  ARGS=%ARGS% -video_size "1280x720" -framerate "10.0" -pixel_format "yuyv422"  -i video="Integrated Camera"
+::set  ARGS=%ARGS% -video_size "640x480"  -framerate "15.0" -pixel_format "yuyv422"  -i video="USB Camera"
+set  ARGS=%ARGS% -video_size "640x480"  -framerate "30.0" -pixel_format "yuyv422"  -i video="USB Video Device"
+
+::------------------------------------------------------------------------------------------explicitly avoiding other streams but video.
+set  ARGS=%ARGS% -an -sn
+
+
+
+set "D=%DATE%"
+set "T=%TIME%"
+set "YEAR=%D:~-4,4%"
+set "MONTH=%D:~-10,2%"
+set "DAY=%D:~-7,2%"
+set "HOURS=%T:~0,2%"
+set "MINUTES=%T:~3,2%"
+set "SECONDS=%T:~6,2%"
+set "TIMESTAMP=%YEAR%%MONTH%%DAY%_%HOURS%%MINUTES%%SECONDS%"
+set "FILENAME=%~sdp0VID_%TIMESTAMP%.mp4"
+
+set  ARGS=%ARGS% "%FILENAME%"
+
+echo.
+echo ffmpeg %ARGS%    1>&2
+echo.
+call ffmpeg %ARGS%
+echo.
+
+pause
+
