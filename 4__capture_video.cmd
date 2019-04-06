93c8cf11f8ee5ed4fe2278747713a8799b051276
Elad Karako <eladkarako@users.noreply.github.com>
2019-04-07 02:18:04 +0300

    adding desktop-capture

--- 4__capture_video.cmd
+++ 4__capture_video.cmd
@@ -5,6 +5,7 @@ set  ARGS=%ARGS% -y -hide_banner -loglevel "info" -strict "experimental"
 set  ARGS=%ARGS% -rtbufsize 200M
 set  ARGS=%ARGS% -f dshow 
 ::------------------------------------------------------------------------------------------video source arguments - need to use a format that is supported (query all devices and then your selected-device's formats).  -- when switching, uncomment the two-lines.
+::set  ARGS=%ARGS% -video_size "1366x768" -framerate "30.0" -pixel_format "yuv420p"  -i video="screen-capture-recorder"
 ::set  ARGS=%ARGS% -video_size "1280x720" -framerate "10.0" -pixel_format "yuyv422"  -i video="Integrated Camera"
 ::set  ARGS=%ARGS% -video_size "640x480"  -framerate "15.0" -pixel_format "yuyv422"  -i video="USB Camera"
 set  ARGS=%ARGS% -video_size "640x480"  -framerate "30.0" -pixel_format "yuyv422"  -i video="USB Video Device"
